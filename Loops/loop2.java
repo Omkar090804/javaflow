c40aa0c7606483aec7bdda13b1cc2b96d10d0da1
Omkar Darekar <142470834+Omkar090804@users.noreply.github.com>
2024-07-09 18:52:50 +0530

    Update loop2.java

--- Loops/loop2.java
+++ Loops/loop2.java
@@ -57,3 +57,34 @@ public class SCLoop3
     }
     
 }
+
+
+
+
+/*Arithematic Progression */
+
+import java.util.Scanner;
+public class loop3{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("the AP series is");
+        System.out.println("enter the a,d,n");
+        int a= sc.nextInt ();
+        int d= sc.nextInt ();
+        int n= sc.nextInt ();
+
+        for(int i=0;i<n;i++){
+            System.out.print(a+ " , ");
+            a=a+d;
+
+        }
+    }
+}
+
+
+
+
+
+
+
+
