665ee528b0b661a8e7d085d21c6041b81f97cb5a
Omkar Darekar <142470834+Omkar090804@users.noreply.github.com>
2024-07-09 19:25:10 +0530

    Update loop2.java

--- Loops/loop2.java
+++ Loops/loop2.java
@@ -81,7 +81,30 @@ public class loop3{
     }
 }
 
+/*FIBONNACI /*
 
+import java.util.Scanner;
+ public class loop3{
+     public static void main(String[] args) {
+         Scanner sc = new Scanner(System.in);
+         System.out.println("the Fibonnaci series is");
+         System.out.println("enter n");
+         int n= sc.nextInt ();
+         int a = 0;
+         int b = 1;
+         int c;
+         
+         System.out.println(a);
+         System.out.println(b);
+
+         for (int i = 0; i < n - 2; i++) {
+             c = a + b;
+             System.out.println(c);
+             a = b;
+             b = c;
+         }
+     }
+ }
 
 
 
