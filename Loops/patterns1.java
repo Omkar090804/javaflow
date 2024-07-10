5634385222d43e77e130add7c345c4bcec2d90a7
Omkar Darekar <142470834+Omkar090804@users.noreply.github.com>
2024-07-10 14:52:08 +0530

    Create patterns1.java

---
+++ Loops/patterns1.java
@@ -0,0 +1,92 @@
+
+import java.util.Scanner;
+/*public class loop4 {
+    public static void main(String[] args) {
+        for(int i = 1; i <= 5; i++) {
+            for(int j=1;j<=5;j++){
+               System.out.print(j + " ") ;
+            }
+            System.out.println(" ");
+
+        }
+    }
+}
+*/
+
+/* prints
+1 2 3 4 5
+1 2 3 4 5
+1 2 3 4 5
+1 2 3 4 5
+1 2 3 4 5
+ */
+
+
+import java.util.Scanner;
+/* public class loop4 {
+    public static void main(String[] args) {
+        for(int i = 1; i <= 5; i++) {
+            for(int j=1;j<=5;j++){
+               System.out.print(i + " ") ;
+            }
+            System.out.println("");
+
+        }
+    }
+}
+*/
+/*will print
+        1 1 1 1 1
+        2 2 2 2 2
+        3 3 3 3 3
+        4 4 4 4 4
+        5 5 5 5 5
+*/
+
+import java.util.Scanner;
+/*public class loop4 {
+    public static void main(String[] args) {
+        for(int i = 1; i <= 5; i++) {
+            for(int j=1;j<=5;j++){
+               System.out.print(i+j +" ") ;
+
+            }
+            System.out.println("");
+
+        }
+    }
+}
+*/
+/* will print
+         2 3 4 5 6
+         3 4 5 6 7
+         4 5 6 7 8
+         5 6 7 8 9
+         6 7 8 9 10
+*/
+
+
+import java.util.Scanner;
+/*public class loop4 {
+    public static void main(String[] args) {
+        int count=0;
+        for(int i = 1; i <= 5; i++) {
+            for(int j=1;j<=5;j++){
+               System.out.print(++count +" ") ;
+
+            }
+            System.out.println("");
+
+        }
+    }
+}
+*/
+/*will print
+        1 2 3 4 5
+        6 7 8 9 10
+        11 12 13 14 15
+        16 17 18 19 20
+        21 22 23 24 25
+
+ */
+
