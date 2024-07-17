dc25fbd3e8292c86e97472705a1294e2a72727a3
Omkar Darekar <142470834+Omkar090804@users.noreply.github.com>
2024-07-17 17:54:30 +0530

    Update arrays1.java

--- arrays1.java
+++ arrays1.java
@@ -55,4 +55,35 @@ public class Array
         System.out.println("");
 
     }   */
-}
+
+
+/* Copying an array
+    public class array1 {
+        public static void main(String[] args) {
+
+
+            int A[] = new int[6];
+            A[0] = 3;
+            A[1] = 9;
+            A[2] = 7;
+            A[3] = 8;
+            A[4] = 12;
+            A[5] = 6;
+            int B[] = new int[6];
+
+            for (int i = 0; i < A.length; i++)
+                System.out.print(A[i] + ",");
+            System.out.println("");
+
+            for (int i = 0; i > A.length; i++) {
+                A[i] = B[i];
+            }
+            for (int i = 0; i < A.length; i++)
+                System.out.print(A[i] + ",");
+            System.out.println("This is the B array");
+
+
+        }
+    }
+   
+ */
