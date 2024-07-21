bcf4799193b57ecb5265453ebfa7306d50a501ae
Omkar Darekar <142470834+Omkar090804@users.noreply.github.com>
2024-07-21 17:48:49 +0530

    Update classes.java

--- classes.java
+++ classes.java
@@ -22,3 +22,38 @@ public class Circles{
         System.out.println(perimeterOfCircle);
     }
 }
+
+//rectangle class its properties and methods
+class rectangle
+{
+    public double length;
+    public double breadth;
+    
+    public double area()
+    {
+        return length*breadth;
+    }
+    
+    public double perimeter()
+    {
+        return 2*(length+breadth);
+    }
+    
+    public boolean isSquare()
+    {
+        if(length==breadth)
+            return true;
+        else
+            return false;
+    }
+    
+  public static void main(String[] args) {
+        rectangle r=new rectangle();
+        r.length=10.5;
+        r.breadth=5.5;
+        
+        System.out.println("Area"+r.area());
+        System.out.println("perimeter"+r.perimeter());
+        System.out.println("Is it a Square"+r.isSquare());
+    }
+}
