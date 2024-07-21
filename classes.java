763a90bda448beb1f14e6735ad5745cf73cb7f53
Omkar Darekar <142470834+Omkar090804@users.noreply.github.com>
2024-07-21 18:14:16 +0530

    Update classes.java

--- classes.java
+++ classes.java
@@ -51,9 +51,49 @@ class rectangle
         rectangle r=new rectangle();
         r.length=10.5;
         r.breadth=5.5;
-        
-        System.out.println("Area"+r.area());
-        System.out.println("perimeter"+r.perimeter());
-        System.out.println("Is it a Square"+r.isSquare());
+
+
+      //student class
+class Student {
+    public int roll;
+    public String name;
+    public String course;
+    public int m1, m2, m3;
+
+    public int total() {
+        return m1 + m2 + m3;
     }
+
+    public float average() {
+        return (float) total() / 3;
+    }
+
+    public char grade() {
+        if (average() >= 60)
+            return 'A';
+        else
+            return 'B';
+    }
+
+    public String toString() {
+        return "Roll No:" + roll + "\n" + "Name:" + name + "\n" + "Course:" + course + "\n";
+    }
+
+    public static void main(String[] args) {
+        Student s = new Student();
+
+        s.roll = 1;
+        s.name = "OMKAR";
+        s.course = "CS";
+        s.m1 = 70;
+        s.m2 = 80;
+        s.m3 = 65;
+
+        System.out.println("Total :" + s.total());
+        System.out.println("Average :" + s.average());
+
+        System.out.println("Details:\n " + s);
+
+    }
+
 }
