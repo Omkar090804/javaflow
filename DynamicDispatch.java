b95bcdf9bee7f1eb0b5828d8463b12436b1f43a9
Omkar Darekar <142470834+Omkar090804@users.noreply.github.com>
2024-07-25 13:00:41 +0530

    Update DynamicDispatch.java

--- DynamicDispatch.java
+++ DynamicDispatch.java
@@ -38,3 +38,41 @@ public class DynamicDispatch
 
     }
 }
+
+//example 2
+
+class Car
+{
+    public void start(){
+        System.out.println("Car Started");
+    }
+    public void accelerate(){
+        System.out.println("Car is Accelerated");
+    }
+    public void changeGear(){
+        System.out.println("Car Gear Changed");
+    }
+    
+}
+
+class LuxaryCar extends Car
+{
+    public void changeGear(){
+        System.out.println("Automatic Gear");
+    }
+    public void openRoof(){
+        System.out.println("Sun Roof is Opened");
+    }
+
+    
+    public static void main(String[] args) 
+    {
+        Car c=new LuxaryCar();
+        c.start();
+        c.accelerate();
+        c.changeGear();
+      
+    }
+    
+}
+
