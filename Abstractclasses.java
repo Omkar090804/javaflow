b9e113e4759e148ebb0f5c6cb5072a4021dee5b7
Omkar Darekar <142470834+Omkar090804@users.noreply.github.com>
2024-07-25 21:35:06 +0530

    Update Abstractclasses.java

--- Abstractclasses.java
+++ Abstractclasses.java
@@ -29,3 +29,50 @@ class Sub extends Super
         s.meth2();
     }
 }
+
+//example 2
+abstract class Shape
+{
+   abstract public double perimeter();
+   abstract public double area();
+}
+
+class Circle extends Shape
+{
+    double radius;
+    
+    public double perimeter()
+    {
+        return 2*Math.PI*radius;
+    }
+    public double area()
+    {
+        return Math.PI*radius*radius;
+    }
+            
+}
+        
+class Rectangle extends Shape
+{
+    double length;
+    double breadth;
+    
+    public double perimeter()
+    {
+        return 2*(length+breadth);
+    }
+    public double area()
+    {
+        return length*breadth;
+    }
+            
+ public static void main(String[] args) 
+    {
+        Rectangle r=new Rectangle();
+        r.length=10;
+        r.breadth=5;
+        
+        System.out.println(s.area());
+        
+    }  
+}
