b0954e27306e6aabe95644dd7b9ed50aa09c9df2
Omkar Darekar <142470834+Omkar090804@users.noreply.github.com>
2024-07-26 13:28:48 +0530

    Update interface.java

--- interface.java
+++ interface.java
@@ -48,3 +48,57 @@ class SmartPhone extends Phone implements ICamera,IMusicPlayer
     }
 
 }
+
+
+//example 2
+
+interface Member
+{
+    public void callback();
+}
+
+class Store
+{
+    Member members[]=new Member[10];
+    int count=0;
+       
+    void register(Member m)
+    {
+        members[count++]=m;
+    }
+    void inviteSale()
+    {
+        for(int i=0;i<count;i++)
+            members[i].callback();
+    }
+    
+}
+
+class Customer implements Member
+{
+    String name;
+    
+    Customer(String n)
+    {
+        name=n;
+    }
+    public void callback()
+    {
+        System.out.println("Ok, I will visit,"+name);
+        
+        
+    }
+    
+    public static void main(String[] args) 
+    {
+        Store s=new Store();
+        Customer c1=new Customer("John");
+        Customer c2=new Customer("Smith");
+        
+        s.register(c1);
+        s.register(c2);
+        
+        s.inviteSale();
+    }
+    
+}
