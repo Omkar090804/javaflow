8e25c471b0df7df5df58afdc91fab6e86bc774b5
Omkar Darekar <142470834+Omkar090804@users.noreply.github.com>
2024-07-26 12:46:07 +0530

    Update interface.java

--- interface.java
+++ interface.java
@@ -1,7 +1,11 @@
-class Phone 
+class Phone
 {
-    public void call() { System.out.println("Phone call"); }
-    public void sms() { System.out.println("Phone sending SMS"); }
+    public void call() {
+        System.out.println("Phone call");
+    }
+    public void sms() {
+        System.out.println("Phone sending SMS");
+    }
 }
 
 interface ICamera
@@ -18,23 +22,29 @@ interface IMusicPlayer
 
 class SmartPhone extends Phone implements ICamera,IMusicPlayer
 {
-    public void videoCall() { System.out.println("Smart Phone video calling"); }
-   
-    public void click() { System.out.println("Smart Phone Clicking Photo"); }
-    public void record() { System.out.println("Smart Phone recording video"); }
-    public void play() { System.out.println("Smart Phone playing music"); }
-    public void stop() { System.out.println("Smart Phone stopped playing music"); }
-
-}
-public class InterfaceExample 
-{
-
-    public static void main(String[] args) 
-    {
-        IMusicPlayer sp=new SmartPhone();
-        sp.play();
-        sp.stop();
-        
+    public void videoCall() {
+        System.out.println("Smart Phone video calling");
     }
-    
+
+    public void click() {
+        System.out.println("Smart Phone Clicking Photo");
+    }
+    public void record() {
+        System.out.println("Smart Phone recording video");
+    }
+    public void play() {
+        System.out.println("Smart Phone playing music");
+    }
+    public void stop() {
+        System.out.println("Smart Phone stopped playing music");
+    }
+
+    public static void main(String[] args)
+    {
+        ICamera sp= new SmartPhone();
+        sp.click();
+        sp.record();
+
+    }
+
 }
