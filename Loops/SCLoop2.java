2f6c368193f42829761ea3fdd6c1045b7f366f76
Omkar Darekar <142470834+Omkar090804@users.noreply.github.com>
2024-08-03 23:17:33 +0530

    Update SCLoop2.java

--- Loops/SCLoop2.java
+++ Loops/SCLoop2.java
@@ -4,7 +4,7 @@ public class SCLoop2 {
 
    /* display number of digits /*
 
-    public static void main(String[] args)
+public static void main(String[] args)
     {
        Scanner scan=new Scanner(System.in);
 
