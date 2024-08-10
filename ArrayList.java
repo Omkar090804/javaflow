66b022ec5cfb5c8b4cca1221b87f574fc29384cd
Omkar Darekar <142470834+Omkar090804@users.noreply.github.com>
2024-08-11 00:23:01 +0530

    Update ArrayList.java

--- ArrayList.java
+++ ArrayList.java
@@ -1,5 +1,3 @@
-//ArrayList and Iterator 
-
 import java.util.*;
 
 class Main {
@@ -7,9 +5,19 @@ class Main {
         ArrayList<Integer> Al1 = new ArrayList<>();
         ArrayList<Integer> Al2 = new ArrayList<>(List.of(10, 20, 30, 40, 50));
         Al1.add(50);
-        Al1.add(0,5);
         Al1.addAll(Al2);
+        Al1.set(2,100);
+        Al1.add(0,5);
+        System.out.println(Al1.contains(7));
+        System.out.println(Al1.get(3));
+        System.out.println(Al1.indexOf(30));
+        System.out.println(Al1.isEmpty());
+        System.out.println(Al1.clone());
         System.out.println(Al1);
         System.out.println(Al2);
+
+        for (int i=0;i<Al1.size();i++){
+            System.out.println(Al1.get(i));
+        }
     }
 }
