b9bc8b8264fcb3c7bfd8c52737371cc38b21368e
Omkar Darekar <142470834+Omkar090804@users.noreply.github.com>
2024-09-15 10:36:11 +0530

    Update trycatch.java

--- trycatch.java
+++ trycatch.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class ExceptionDemo 
 {
     public static void main(String[] args) 
-    {
+    { 
         int a,b,c;
         
         Scanner sc=new Scanner(System.in);
