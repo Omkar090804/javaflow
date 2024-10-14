89a59012bd6dbc3083b2753ba2eb4d0c99d16cac
Omkar Darekar <142470834+Omkar090804@users.noreply.github.com>
2024-10-14 13:36:46 +0530

    Create AWT_01.java

---
+++ AWT_01.java
@@ -0,0 +1,24 @@
+import java.awt.*;
+
+public class AWT
+{
+    static  int count=0;
+
+    public static void main(String[] args)
+    {
+        Frame f=new Frame();
+        f.setTitle("My First Window App");
+        Button b=new Button("Click Me");
+        f.add(b);
+        TextField tf=new TextField(20);
+        f.add(tf);
+
+        b.addActionListener((e)->tf.setText("Clicked "+count++));
+
+        f.setLayout(new FlowLayout());
+        f.setSize(500,500);
+        f.setVisible(true);
+
+    }
+
+}
