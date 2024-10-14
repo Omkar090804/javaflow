68f3f0389b859b320cab39f1c3a990224448ad02
Omkar Darekar <142470834+Omkar090804@users.noreply.github.com>
2024-10-14 14:25:32 +0530

    Update AWT_01.java

--- AWT_01.java
+++ AWT_01.java
@@ -1,10 +1,10 @@
 import java.awt.*;
 
-public class AWT
+public class AWT                                             
 {
     static  int count=0;
 
-    public static void main(String[] args)
+    public static void main(String[] args)           
     {
         Frame f=new Frame();
         f.setTitle("My First Window App");
@@ -21,4 +21,37 @@ public class AWT
 
     }
 
+}
+
+
+//like this 
+import java.awt.*;
+
+public class FirstApp extends Frame             // we can create our own class FirstApp which extends frame and declare label,button,textfeild there            
+{
+    Label l;
+    TextField tf;
+    Button b;
+    
+    public FirstApp()                   //then in its construcor we give them values and then in main methods we ca add size and visiblity using object
+    {
+        super("My App");
+        
+        setLayout(new FlowLayout());
+        
+        l=new Label("Name");
+        tf=new TextField(20);
+        b=new Button("OK");
+        
+        add(l);
+        add(tf);
+        add(b);   
+        
+    }  
+    public static void main(String[] args) 
+    {
+        FirstApp f=new FirstApp();
+        f.setSize(400,400);
+        f.setVisible(true);
+    } 
 }
