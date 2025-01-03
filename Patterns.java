7bb259ee5346176cc8eeba8ac256ee8b00488dee
Omkar Darekar <142470834+Omkar090804@users.noreply.github.com>
2025-01-03 20:06:30 +0530

    Create Patterns.java

---
+++ Patterns.java
@@ -0,0 +1,114 @@
+*****
+*****
+*****
+*****
+*****
+
+
+class Patterns{
+    public static void main(String[] args){
+        for(int i=1;i<6;i++){
+            for(int j=1;j<6;j++){
+                System.out.print("*");
+
+            }
+            System.out.println();
+
+        }
+
+    }
+}
+
+
+
+*
+**
+***
+****
+*****
+
+  class Patterns{
+    public static void main(String[] args){
+        for(int i=1;i<6;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+
+            }
+            System.out.println();
+
+        }
+
+    }
+}
+
+
+
+1
+12
+123
+1234
+12345
+
+  class Patterns{
+    public static void main(String[] args){
+        for(int i=1;i<6;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(j);
+
+            }
+            System.out.println();
+
+        }
+
+    }
+}
+
+
+
+1
+22
+333
+4444
+55555
+
+  class Patterns{
+    public static void main(String[] args){
+        for(int i=1;i<6;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(i);
+
+            }
+            System.out.println();
+
+        }
+
+    }
+}
+
+
+
+12345
+1234
+123
+12
+1
+
+  class Patterns{
+    public static void main(String[] args){
+        for(int i=5;i>0;i--){
+            for(int j=1;j<=i;j++){
+                System.out.print(j);
+
+            }
+            System.out.println();
+
+        }
+
+    }
+}
+
+
+
+
+
+  
