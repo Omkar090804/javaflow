b288c92b7176c8ac1313ae80c12172cfedd07f9e
Omkar Darekar <142470834+Omkar090804@users.noreply.github.com>
2025-01-08 19:57:13 +0530

    Update Patterns1.java

--- Patterns1.java
+++ Patterns1.java
@@ -149,3 +149,32 @@ class Patterns {
 }
 
 
+
+
+1
+23
+456
+78910
+
+    import java.util.Scanner;
+
+class Patterns {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a number (n): ");
+        int n = scanner.nextInt();
+        int start=1;
+        for (int i = 1; i <= n; i++) {
+
+            for (int j = 1; j <= i; j++) {
+                System.out.print(start);
+                start++;
+
+            }
+
+            System.out.println();
+        }
+
+    }
+}
