31da2c470eff80417090de8d282d1a90b115f6f6
Omkar Darekar <142470834+Omkar090804@users.noreply.github.com>
2025-01-25 08:59:15 +0530

    Update Recursion.java

--- Recursion.java
+++ Recursion.java
@@ -1,1 +1,39 @@
+//Sorting an Array using recursion  //sprting stack also same logic just intead of add,remove use push pop ,peek
 
+import java.util.ArrayList;
+
+public class SortUsingRecursion {
+    public static void sort(ArrayList<Integer> v) {
+        if (v.size() <= 1) {
+            return;
+        }
+        int temp = v.remove(v.size() - 1);
+        sort(v);
+        insert(v,temp);
+    }
+    public static void insert(ArrayList<Integer> v,int temp) {
+    if(v.size()==0 || v.get(v.size()-1)<=temp) {
+    v.add(temp);
+    return;
+    
+        }
+
+        int val= v.remove(v.size() - 1);    //val is created to reduce the space,if not recursion will move on temp continuously
+        insert(v,temp);
+        v.add(val);
+
+    }
+
+    public static void main (String[]args){
+            ArrayList<Integer> v = new ArrayList<>();
+            v.add(5);
+            v.add(1);
+            v.add(3);
+            v.add(2);
+            v.add(4);
+
+            System.out.println("Before sorting: " + v);
+            sort(v);
+            System.out.println("After sorting: " + v);
+        }
+    }
