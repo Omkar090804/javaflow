0f6e605b726a0a1f6dbcab8bb18480d539167a24
Omkar Darekar <142470834+Omkar090804@users.noreply.github.com>
2025-03-31 01:09:33 +0530

    Update KClosestPointsToOrigin.java

--- KClosestPointsToOrigin.java
+++ KClosestPointsToOrigin.java
@@ -1,4 +1,4 @@
-//here we use max heap because max heep keeps the largest element at the top and we also pop from top so largest element that is the farthest from the origin will be poped and we will get the closest to the origin at the bottom of the heap
+//here we use max heap because max heep keeps the largest element at the top and we also pop from top so largest element that is the farthest from the origin will be popped and we will get the closest to the origin at the bottom of the heap
 
 class Solution {
     public int[][] kClosest(int[][] points, int k) {
