64a5f87a8936ba4d842468be91924b9f422cdd0f
Omkar Darekar <142470834+Omkar090804@users.noreply.github.com>
2025-06-16 22:29:19 +0530

    Update RodCuttingProblem.java

--- DP/RodCuttingProblem.java
+++ DP/RodCuttingProblem.java
@@ -1,4 +1,12 @@
 //Unbounded Knapsack Problem
+/* 
+Given a rod of length n inches and an array price[], where price[i] denotes the value of a piece of length i. Your task is to determine the maximum value obtainable by cutting up the rod and selling the pieces.
+
+Example:
+Input: price[] = [1, 5, 8, 9, 10, 17, 17, 20]
+Output: 22
+Explanation: The maximum obtainable value is 22 by cutting in two pieces of lengths 2 and 6, i.e., 5 + 17 = 22.
+*/
 
 class Solution {
     public int cutRod(int[] price) {
