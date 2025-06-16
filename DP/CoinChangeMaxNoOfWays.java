423adfd6eae8cfadef10720a69f72b14b8303ece
Omkar Darekar <142470834+Omkar090804@users.noreply.github.com>
2025-06-17 01:43:07 +0530

    Create CoinChangeMaxNoOfWays.java

---
+++ DP/CoinChangeMaxNoOfWays.java
@@ -0,0 +1,28 @@
+
+
+class Solution {
+    public int minCoins(int coins[], int sum) {
+        int n = coins.length;
+        int[][] t = new int[n+1][sum+1];
+        
+        for(int i = 0; i <= n; i++){
+            t[i][0] = 0;
+        }
+        for(int j = 1; j <= sum; j++){
+            t[0][j] = sum + 1;
+        }
+       
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= sum; j++){
+                if(coins[i-1] <= j){
+                    t[i][j] = Math.min(1 + t[i][j-coins[i-1]], t[i-1][j]);
+                }
+                else{
+                    t[i][j] = t[i-1][j];
+                }
+            }
+        }
+        
+        return t[n][sum] == sum + 1 ? -1 : t[n][sum];
+    }
+}
