0b1c120bcbacc66dc2f06fdab78e706b4d62479f
Omkar Darekar <142470834+Omkar090804@users.noreply.github.com>
2025-06-21 20:06:30 +0530

    Create printLongestCommonSubsequence.java

---
+++ DP/printLongestCommonSubsequence.java
@@ -0,0 +1,33 @@
+class Solution {
+    public static String findLCS(int n, int m, String s1, String s2) {
+        int[][] dp = new int[n + 1][m + 1];
+
+        // Fill the dp table
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (s1.charAt(i - 1) == s2.charAt(j - 1))
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                else
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+
+        // Reconstruct the LCS string
+        int i = n, j = m;
+        StringBuilder lcs = new StringBuilder();
+
+        while (i > 0 && j > 0) {
+            if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
+                lcs.append(s1.charAt(i - 1));
+                i--;
+                j--;
+            } else if (dp[i - 1][j] > dp[i][j - 1]) {
+                i--;
+            } else {
+                j--;
+            }
+        }
+
+        return lcs.reverse().toString();
+    }
+}
