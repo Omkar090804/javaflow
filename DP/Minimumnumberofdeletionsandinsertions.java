f4c491318d95879a6b943eb4eff980b110628428
Omkar Darekar <142470834+Omkar090804@users.noreply.github.com>
2025-06-22 02:06:45 +0530

    Create Minimumnumberofdeletionsandinsertions.java

---
+++ DP/Minimumnumberofdeletionsandinsertions.java
@@ -0,0 +1,28 @@
+// User function Template for Java
+
+class Solution {
+    public int minOperations(String s1, String s2) {
+        
+        int n=s1.length();
+        int m=s2.length();
+        
+        int t[][] = new int[n+1][m+1];
+        
+        for(int i=1;i<n+1;i++){
+            for(int j=1;j<m+1;j++){
+                if(s1.charAt(i-1)==s2.charAt(j-1)){
+                    t[i][j]=1+t[i-1][j-1];
+                }
+                else{
+                    t[i][j]=Math.max(t[i-1][j],t[i][j-1]);
+                }
+            }
+        }
+        
+        int lcs=t[n][m];
+        int deletions=n-lcs;
+        int insertions=m-lcs;
+       
+        return  deletions + insertions;
+    }
+}
