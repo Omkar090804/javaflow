6d028e369eaf785e38d0804dd79747f0e98ce713
Omkar Darekar <142470834+Omkar090804@users.noreply.github.com>
2025-06-27 00:28:22 +0530

    Update MatrixChainMultiplication.java

--- DP/MatrixChainMultiplication.java
+++ DP/MatrixChainMultiplication.java
@@ -1,12 +1,15 @@
-//Matrix Chain Multiplication
-
-
-
+/* Matrix Chain Multiplication steps
+1) take values of i,j
+2) Write a base condition 
+3) Create a loop for k according to situatiion 
+4) find temp ans which we need to calculate for all values of k and then out of them we take the minimum as answer as we need min cost
+5) printting minimum cost
+*/
 
 public class MatrixChainMultiplication {
 
     public static int solve(int[] arr, int i, int j) {
-        if (i >= j)
+        if (i >= j)                                            
             return 0;
 
         int ans = Integer.MAX_VALUE;
