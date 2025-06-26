af28d8fdddab9391974add57ce1ad68356cd5cff
Omkar Darekar <142470834+Omkar090804@users.noreply.github.com>
2025-06-27 03:34:28 +0530

    Create PalindromePartitioning.java

---
+++ DP/PalindromePartitioning.java
@@ -0,0 +1,60 @@
+/
+
+class Solution {
+
+    static int[][] t;
+    
+    static int palPartition(String s) {
+        int n = s.length();
+        
+        t = new int[n+1][n+1];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                t[i][j] = -1;
+            }
+        }
+
+        return solve(s, 0, n - 1);
+    }
+
+    static boolean isPalindrome(String s, int i, int j) {
+        while (i <= j) {
+            if (s.charAt(i) != s.charAt(j))
+                return false;
+            i++;
+            j--;
+        }
+        return true;
+    }
+
+    static int solve(String s, int i, int j) {
+        if (t[i][j] != -1)
+            return t[i][j];
+
+        if (i >= j || isPalindrome(s, i, j)) {
+            return t[i][j] = 0;
+        }
+
+        int minCuts = Integer.MAX_VALUE;
+
+        for (int k = i; k < j; k++) {
+            int left, right;
+
+            if (t[i][k] != -1)
+                left = t[i][k];
+            else
+                left = solve(s, i, k);
+
+            if (t[k + 1][j] != -1)
+                right = t[k + 1][j];
+            else
+                right = solve(s, k + 1, j);
+
+            int temp = left + right + 1;
+            minCuts = Math.min(minCuts, temp);
+        }
+
+        return t[i][j] = minCuts;
+    }
+}
