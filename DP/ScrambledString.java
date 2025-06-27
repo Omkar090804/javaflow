b068079ec426e571a7e21fcbb2bd93c075b4d969
Omkar Darekar <142470834+Omkar090804@users.noreply.github.com>
2025-06-28 01:20:54 +0530

    Update ScrambledString.java

--- DP/ScrambledString.java
+++ DP/ScrambledString.java
@@ -1,3 +1,37 @@
+/*
+Given two strings S1 and S2 of equal length, the task is to determine if S2 is a scrambled form of S1.
+
+Scrambled string: Given string str, we can represent it as a binary tree by partitioning it into two non-empty substrings recursively.
+Below is one possible representation of str = coder:
+ To scramble the string, we may choose any non-leaf node and swap its two children. 
+Suppose, we choose the node co and swap its two children, it produces a scrambled string ocder.
+Similarly, if we continue to swap the children of nodes der and er, it produces a scrambled string ocred.
+
+Note: Scrambled string is not the same as an Anagram.
+
+Print "Yes" if S2 is a scrambled form of S1 otherwise print "No".
+
+Example 1:
+
+Input: S1="coder", S2="ocder"
+Output: Yes
+Explanation: ocder is a scrambled 
+form of coder.
+
+    ocder
+   /    \
+  oc    der
+ / \    
+o   c  
+
+As "ocder" can represent it 
+as a binary tree by partitioning 
+it into two non-empty substrings.
+We just have to swap 'o' and 'c' 
+to get "coder".
+
+*/
+
 import java.util.HashMap;
 
 class Solution {
