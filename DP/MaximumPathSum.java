d5aa5a1d483012b64ca599a33c3e31c141c764be
Omkar Darekar <142470834+Omkar090804@users.noreply.github.com>
2025-06-30 02:25:56 +0530

    Create MaximumPathSum.java

---
+++ DP/MaximumPathSum.java
@@ -0,0 +1,29 @@
+
+class Solution {
+    public int res;
+
+    public int solve(Node root) {
+        if (root == null) {
+            return 0;
+        }
+        
+        int left = Math.max(0, solve(root.left));
+        int right = Math.max(0, solve(root.right));
+        
+    
+        int temp = root.data + Math.max(left, right);
+        
+
+        int answer = root.data + left + right;
+
+        res = Math.max(res, answer);
+
+        return temp;
+    }
+
+    public int findMaxSum(Node node) {
+        res = Integer.MIN_VALUE;
+        solve(node);
+        return res;
+    }
+}
