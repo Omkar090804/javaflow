9e081adaf612ce122ffe6d35f4ded26ffcc95474
Omkar Darekar <142470834+Omkar090804@users.noreply.github.com>
2025-06-30 01:50:22 +0530

    Update DiameterOfABinaryTree.java

--- DP/DiameterOfABinaryTree.java
+++ DP/DiameterOfABinaryTree.java
@@ -1,4 +1,10 @@
+/*
+Given a binary tree, the diameter (also known as the width) is defined as the number of edges on the longest path between two leaf nodes in the tree. This path may or may not pass through the root. Your task is to find the diameter of the tree.
 
+Examples:
+
+
+    
 class Solution {
     public int res; 
 
