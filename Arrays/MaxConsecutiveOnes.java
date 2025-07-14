a9386644a8d74b4d2ecbac9294c3d51c60dbd6a2
Omkar Darekar <142470834+Omkar090804@users.noreply.github.com>
2025-07-14 23:41:29 +0530

    Update MaxConsecutiveOnes.java

--- Arrays/MaxConsecutiveOnes.java
+++ Arrays/MaxConsecutiveOnes.java
@@ -15,3 +15,33 @@ class Solution {
         return maxcount;
     }
 }
+
+
+//if both one and zeros 
+
+class Solution {
+    public int maxConsecBits(int[] arr) {
+       
+        int countones=0;
+        int maxcountones=0;
+        int countzeros=0;
+        int maxcountzeros=0;
+        
+        for(int i=0;i<arr.length;i++){
+         if (arr[i]==1){
+            countones++;
+            countzeros=0;
+         }
+         else{
+            countzeros++;
+            countones=0;
+         }
+          maxcountones = Math.max(maxcountones, countones);
+          maxcountzeros=Math.max(maxcountzeros,countzeros);
+        }
+        return Math.max(maxcountones,maxcountzeros);
+    }
+}
+        
+    
+
