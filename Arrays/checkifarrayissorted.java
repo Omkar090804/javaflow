575586bea299c1099668d73d83f4914637bb4a47
Omkar Darekar <142470834+Omkar090804@users.noreply.github.com>
2025-07-14 22:24:22 +0530

    Create checkifarrayissorted.java

---
+++ Arrays/checkifarrayissorted.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean arraySortedOrNot(int[] arr) {
+        
+        for(int i=0;i<arr.length-1;i++){
+            if(arr[i]>arr[i+1]){
+            return false;
+            }
+            
+        }
+        return true;
+    }
+}
