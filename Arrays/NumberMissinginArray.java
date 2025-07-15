c913657bbf6d90072805569cb775a923f27d1cfd
Omkar Darekar <142470834+Omkar090804@users.noreply.github.com>
2025-07-15 11:22:58 +0530

    Update NumberMissinginArray.java

--- Arrays/NumberMissinginArray.java
+++ Arrays/NumberMissinginArray.java
@@ -3,7 +3,7 @@ import java.util.*;;
 class Solution {
     int missingNum(int arr[]) {
        Arrays.sort(arr);
-       int count=arr[0];
+       int count=1;
        for(int i=0;i<arr.length;i++){
            if(arr[i]!=count){
                return count;
